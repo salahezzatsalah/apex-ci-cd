195861c53391d10d8ed822c8515c07c96df8334b
salahezzatsalah <161888314+salahezzatsalah@users.noreply.github.com>
2026-03-26 10:54:39 +0200

    Update deployment scripts with safe ORDS check

--- dist/releases/ords/admin/ords.sql
+++ dist/releases/ords/admin/ords.sql
@@ -1,60 +1,18 @@
--- liquibase formatted sql
--- changeset ADMIN:1774513278847 stripComments:false  logicalFilePath:ords/admin/ords.sql
--- sqlcl_snapshot {"hash":"ac28a61abbef996f7ae237710e3f6e6e8c1fb836","type":"ORDS_SCHEMA","name":"ords","schemaName":"ADMIN","sxml":""}
---
-        
-DECLARE
-  l_roles     OWA.VC_ARR;
-  l_modules   OWA.VC_ARR;
-  l_patterns  OWA.VC_ARR;
-
 BEGIN
-  ORDS.ENABLE_SCHEMA(
-      p_enabled             => TRUE,
-      p_url_mapping_type    => 'BASE_PATH',
-      p_url_mapping_pattern => 'admin',
-      p_auto_rest_auth      => FALSE);
-
-  ORDS.CREATE_ROLE(
-      p_role_name=> 'oracle.dbtools.role.autorest.ADMIN');
-  ORDS.CREATE_ROLE(
-      p_role_name=> 'oracle.dbtools.role.autorest.any.ADMIN');
-  l_roles(1) := 'oracle.dbtools.autorest.any.schema';
-  l_roles(2) := 'oracle.dbtools.role.autorest.ADMIN';
-
-  ORDS.DEFINE_PRIVILEGE(
-      p_privilege_name => 'oracle.dbtools.autorest.privilege.ADMIN',
-      p_roles          => l_roles,
-      p_patterns       => l_patterns,
-      p_modules        => l_modules,
-      p_label          => NULL,
-      p_description    => NULL,
-      p_comments       => NULL); 
-
-  l_roles.DELETE;
-  l_modules.DELETE;
-  l_patterns.DELETE;
-
-  l_roles(1) := 'SODA Developer';
-  l_patterns(1) := '/soda/*';
-
-  ORDS.DEFINE_PRIVILEGE(
-      p_privilege_name => 'oracle.soda.privilege.developer',
-      p_roles          => l_roles,
-      p_patterns       => l_patterns,
-      p_modules        => l_modules,
-      p_label          => NULL,
-      p_description    => NULL,
-      p_comments       => NULL); 
-
-  l_roles.DELETE;
-  l_modules.DELETE;
-  l_patterns.DELETE;
-
-
-COMMIT;
-
+    ORDS.ENABLE_SCHEMA(
+        p_enabled             => TRUE,
+        p_schema              => 'WKSP_ELWAGHA50',
+        p_url_mapping_type    => 'BASE_PATH',
+        p_url_mapping_pattern => 'wksp_elwagha50',
+        p_auto_rest_auth      => FALSE
+    );
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE = -20049 THEN
+            NULL; -- Ignore "already enabled" error
+        ELSE
+            RAISE;
+        END IF;
 END;
 /
-
-
