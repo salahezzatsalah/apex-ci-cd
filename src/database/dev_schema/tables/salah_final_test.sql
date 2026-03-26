62216fc932545134ea9e2f54ace782cd76bf775d
salahezzatsalah <161888314+salahezzatsalah@users.noreply.github.com>
2026-03-26 10:21:27 +0200

    Auto-deploying SALAH_FINAL_TEST table - Final CI/CD Test

---
+++ src/database/dev_schema/tables/salah_final_test.sql
@@ -0,0 +1,10 @@
+create table salah_final_test (
+    id   number,
+    name varchar2(100 byte)
+);
+
+alter table salah_final_test add primary key ( id )
+    using index enable;
+
+
+-- sqlcl_snapshot {"hash":"7ac98e90a36cc14cbcb091801157a1b92e71ef20","type":"TABLE","name":"SALAH_FINAL_TEST","schemaName":"DEV_SCHEMA","sxml":"\n  <TABLE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>DEV_SCHEMA</SCHEMA>\n   <NAME>SALAH_FINAL_TEST</NAME>\n   <RELATIONAL_TABLE>\n      <COL_LIST>\n         <COL_LIST_ITEM>\n            <NAME>ID</NAME>\n            <DATATYPE>NUMBER</DATATYPE>\n            \n         </COL_LIST_ITEM>\n         <COL_LIST_ITEM>\n            <NAME>NAME</NAME>\n            <DATATYPE>VARCHAR2</DATATYPE>\n            <LENGTH>100</LENGTH>\n            <COLLATE_NAME>USING_NLS_COMP</COLLATE_NAME>\n            \n         </COL_LIST_ITEM>\n      </COL_LIST>\n      <PRIMARY_KEY_CONSTRAINT_LIST>\n         <PRIMARY_KEY_CONSTRAINT_LIST_ITEM>\n            <COL_LIST>\n               <COL_LIST_ITEM>\n                  <NAME>ID</NAME>\n               </COL_LIST_ITEM>\n            </COL_LIST>\n            <USING_INDEX></USING_INDEX>\n         </PRIMARY_KEY_CONSTRAINT_LIST_ITEM>\n      </PRIMARY_KEY_CONSTRAINT_LIST>\n      <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n      <PHYSICAL_PROPERTIES>\n         <HEAP_TABLE></HEAP_TABLE>\n      </PHYSICAL_PROPERTIES>\n      \n   </RELATIONAL_TABLE>\n</TABLE>"}
